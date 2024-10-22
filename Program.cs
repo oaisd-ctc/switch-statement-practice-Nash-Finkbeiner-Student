51f3d67f99b8341b19dd23e31cdb4c0629651eda
Nash-Finkbeiner-Student <nafi36a@student.oaisd.org>
2024-10-22 12:28:27 +0000

    Had To Fix Text Again

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-    Console.WriteLine("Enter a number");
+    Console.WriteLine("Enter the number");
     int num = int.Parse(Console.ReadLine());
     switch(num)
     {
